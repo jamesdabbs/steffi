295d6151009951557d9a400294b473c88c968adb
James Dabbs <jamesdabbs@gmail.com>
2013-04-11 13:45:32 -0400

    Better constructor test syntax

--- spec/steffi/constructors_spec.rb
+++ spec/steffi/constructors_spec.rb
@@ -1,46 +1,45 @@
 require 'helper'
 
 describe 'Graph constructors' do
-  def self.constructor name, *args
+  def self.constructor name, *args, &block
     it "- #{name}" do
-      # FIXME: why is `should have(n).vertices` syntax not working here?
-      yield Steffi::Graph.send name, *args
+      instance_exec Steffi::Graph.send(name, *args), &block
     end
   end
 
   constructor :atlas, 1 do |graph|
-    graph.vertices.size.should == 1
+    graph.should have(1).vertices
   end
 
   constructor :citations, 55 do |graph|
-    graph.vertices.size.should == 55
+    graph.should have(55).vertices
   end
 
   constructor :erdos_renyi, 100, 123 do |graph|
-    graph.edges.size.should == 123
+    graph.should have(123).edges
   end
 
   constructor :famous, 'meredith' do |graph|
-    graph.vertices.size.should == 70
+    graph.should have(70).vertices
   end
 
   constructor :forest_fire, 111, 0.01, 3, 20 do |graph|
-    graph.vertices.size.should == 111
+    graph.should have(111).vertices
   end
 
   constructor :full, 4 do |graph|
-    graph.edges.size.should == 6
+    graph.should have(6).edges
   end
 
   constructor :ring, 9 do |graph|
-    graph.edges.size.should == 9
+    graph.should have(9).edges
   end
 
   constructor :star, 6 do |graph|
-    graph.edges.size.should == 5
+    graph.should have(5).edges
   end
 
   constructor :tree, 16, 2 do |graph|
-    graph.edges.size.should == 15
+    graph.should have(15).edges
   end
 end
